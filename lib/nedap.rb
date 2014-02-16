deabd7b9d7f979e997f776c4fe780e01b89f7f4d
Apoorv Parijat <apoorvparijat@gmail.com>
2014-02-16 20:13:12 +0530

    Added test for hello_world

--- lib/nedap.rb
+++ lib/nedap.rb
@@ -1,14 +1,17 @@
 class Nedap < BasicObject
-  def initialize &block
+  def initialize output = ::IO.new(2), &block
+    @output = output
     instance_eval &block if ::Kernel.block_given?
   end
 
   def hello_world!
     # ::Kernel.puts 'Hello World!'
-    ::IO.new(2).puts 'Hello World!'
+    @output.puts 'Hello World!'
   end
 end
 
+
+__END__
 Nedap.new do
   hello_world!
   puts 'aap'
--- spec/unit/nedap_spec.rb
+++ spec/unit/nedap_spec.rb
@@ -1,11 +1,17 @@
 require 'nedap'
+require 'stringio'
 
 describe Nedap do
   it 'should be loaded' do
     Nedap.should_not be_nil
   end
 
-  describe '.initialize' do
-    Nedap.new
+  describe '.hello_world!' do
+    it 'should write "Hello World!"' do
+      output =  StringIO.new 
+      Nedap.new(output).hello_world!
+      output.seek(0)
+      output.read.strip.should eq('Hello World!')
+    end
   end
 end
